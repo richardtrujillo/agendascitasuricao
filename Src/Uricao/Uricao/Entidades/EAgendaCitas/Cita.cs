1ccce292f2a3f7208159890f18d8139b062149fe
JhaniaParra <JhaniaParra@localhost>
2012-11-24 20:19:33 -0530

    clase scitas

--- Src/Uricao/Uricao/Entidades/EAgendaCitas/Cita.cs
+++ Src/Uricao/Uricao/Entidades/EAgendaCitas/Cita.cs
@@ -7,5 +7,22 @@ namespace Uricao.Entidades.EAgendaCitas
 {
     public class Cita
     {
+        private DateTime _horaInicio;
+        private DateTime _horaFin;
+        private Boolean _confirmacion;
+        private Boolean _status;
+        private List<Cita> _listaCita;
+
+
+        public Boolean _Status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
+
+
+        public Cita()
+        {
+        }
     }
 }
